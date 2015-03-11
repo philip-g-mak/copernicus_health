ef18b4872dc9d69ee7bab48d9b4ede49a527a016
Tom Nahass <tomnahass@Toms-MBP.home>
2015-03-11 07:27:42 -0400

    changed settings

--- ch/settings.py
+++ ch/settings.py
@@ -15,19 +15,19 @@ MANAGERS = ADMINS
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'copernicus',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': 'nahata5',
-        'PASSWORD': 'yankees',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#         'NAME': 'copernicus',                      # Or path to database file if using sqlite3.
+#         # The following settings are not used with sqlite3:
+#         'USER': 'nahata5',
+#         'PASSWORD': 'yankees',
+#         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+#         'PORT': '',                      # Set to empty string for default.
+#     }
+#}
 
-
+DATABASES = {}
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 
