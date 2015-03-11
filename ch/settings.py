161dc86c847215d2af7372b303eb7aa952883eca
Tom Nahass <tomnahass@Toms-MBP.home>
2015-03-11 07:22:00 -0400

    moved proc

--- ch/settings.py
+++ ch/settings.py
@@ -36,8 +36,10 @@ DATABASES['default']['ENGINE'] = 'django_postgrespool'
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
